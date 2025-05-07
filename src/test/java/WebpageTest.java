eae0f4e62a74c7dc0d6188fff1ff9b81a2fbd06d
nharshitha01 <nharshitha112@gmail.com>
2025-05-07 10:02:15 +0530

    successful

--- src/test/java/WebpageTest.java
+++ src/test/java/WebpageTest.java
@@ -1,4 +1,3 @@
-//package org.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,30 +6,29 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
 public class WebpageTest {
     private static WebDriver driver;
 
     @BeforeTest
     public void openBrowser() throws InterruptedException {
-        // Initialize the WebDriver and open the browser
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        Thread.sleep(2000);  // Wait for the page to load (though it's better to use WebDriverWait)
+         driver = new ChromeDriver();
+         driver.manage().window().maximize();
+        Thread.sleep(2000);
+
         driver.get("https://nharshitha01.github.io/har_gradle_exercise/");
-    }
 
+    }
     @Test
-    public void titleValidationTest() {
-        String actualTitle = driver.getTitle();
+    public void titleValidationTest(){
+         String actualTitle = driver.getTitle();
         String expectedTitle = "Sample Website";
-
-
+        Assert.assertEquals(actualTitle, expectedTitle);
+         assertTrue(true, "Title should contain 'Sample'");
     }
-
     @AfterTest
     public void closeBrowser() throws InterruptedException {
-        // Wait for a short time before quitting the driver
         Thread.sleep(1000);
-        driver.quit();  // Quit the browser
+        driver.quit();
     }
 }
